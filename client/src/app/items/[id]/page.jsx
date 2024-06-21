be79a216c09f020e7ac7eb25905c72f22d74180d
Franco Alfano <flalfano@summit.net.ar>
2024-06-21 16:09:29 -0300

    Hotfix to accurately follow app/ SSR structure for dynamic routes

--- client/src/app/items/[id]/page.jsx
+++ client/src/app/items/[id]/page.jsx
@@ -1,14 +1,13 @@
 'use client';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useRouter } from 'next/router';
-import ItemImage from '../../components/ItemImage/ItemImage';
-import { formatCurrency } from '../../utils/formatCurrency';
+import { useParams } from 'next/navigation';
+import ItemImage from '../../../components/ItemImage/ItemImage';
+import { formatCurrency } from '../../../utils/formatCurrency';
 
 const ProductDetail = () => {
   const [product, setProduct] = useState(null);
-  const router = useRouter();
-  const { id } = router.query;
+  const { id } = useParams();
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -32,16 +31,14 @@ const ProductDetail = () => {
   return (
     <div className="container mx-auto p-4">
       <div className="bg-white p-4 rounded shadow">
-        <div className="flex">
-          <ItemImage src={product.picture} alt={product.title} size="large" />
-          <div className="ml-4">
-            <h1 className="text-2xl font-bold">{product.title}</h1>
-            <p className="text-lg">
-              {formatCurrency(product.price.amount, product.price.currency)}
-            </p>
-            <p>{product.condition}</p>
-            <p>{product.description}</p>
-          </div>
+        <ItemImage src={product.picture} alt={product.title} size="large" />
+        <div className="ml-4">
+          <h1 className="text-2xl font-bold">{product.title}</h1>
+          <p className="text-lg">
+            {formatCurrency(product.price.amount, product.price.currency)}
+          </p>
+          <p>{product.condition}</p>
+          <p>{product.description}</p>
         </div>
       </div>
     </div>
